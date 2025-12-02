c9c0c93074b6c21709e61d275beaa34b67a22088
Jaden505 <jadenxxxl@gmail.com>
2025-12-02 14:00:38 +0100

    route into run.py fix circular import

--- scripts/webhookv2/run.py
+++ scripts/webhookv2/run.py
@@ -2,7 +2,33 @@ from flask import Flask
 
 app = Flask(__name__)
 
-import routes # Ensure routes are imported so that they are registered
+from utils import get_session_id, jsonify_response
+from models import GuessingGameModel
+
+from flask import request
+
+models = {}
+ANIMAL_DATASET_PATH = "animal_dataset.csv"
+
+@app.route("/", methods=["POST"])
+def webhook():
+    req = request.get_json()
+    session_path = req.get("sessionInfo", {}).get("session", "default")
+    session_id = get_session_id(session_path)
+    
+    # Get User Answer
+    params = req.get("sessionInfo", {}).get("parameters", {})
+    user_answer = params.get("last_answer", "start").lower().strip()
+
+    if session_id not in models or user_answer == "reset":
+        models[session_id] = GuessingGameModel(session_id, ANIMAL_DATASET_PATH)
+        user_answer = None
+
+    model = models[session_id]
+    response_text = model.next_response(user_answer)
+
+    return jsonify_response(response_text)
+
         
 if __name__ == "__main__":
     app.run(port=8080)
