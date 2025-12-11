2b628f5ccd6a28526f7ea5c73d1e055dc4255684
Igor1234321-jpg <igordanser@gmail.com>
2025-12-11 14:09:14 +0100

    Added max question limit and asks user what animal they were thinking of when cannot guess itself

--- scripts/webhookv2/responses.py
+++ scripts/webhookv2/responses.py
@@ -1,5 +1,5 @@
 GUESS_RESPONSES = "Are you thinking of a {animal}?"
 STUCK_REASK_RESPONSE = "I'm having trouble guessing your animal. Let's revisit some already asked questions!"
-STUCK_RESET_RESPONSE = "I couldn't guess your animal based on your answers, i'm stuck between these animals: {stuck_animals}."
+STUCK_RESET_RESPONSE = "I couldn't guess your animal based on your answers, i'm stuck between these animals: {stuck_animals}." 
 CORRECT_GUESS_RESPONSE = "yipie, I got it correct! Your animal is indeed a {animal}. Tell me when you want to play again."
 WRONG_GUESS_RESPONSE = "Oh no, I couldn't guess your animal. Let me think of something else."
